98a0c335a5c305947885d5fcc852d479bae2803e
EntaAyano <57655215+EntaAyano@users.noreply.github.com>
2023-01-05 17:22:48 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     カウンター += -1
+    if (カウンター < 0) {
+        カウンター = 4
+    }
     led.plot(カウンター, 2)
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     カウンター += 1
+    if (カウンター > 4) {
+        カウンター = 0
+    }
     led.plot(カウンター, 2)
 })
 let カウンター = 0
